c22d8d90aa908f8d2ff9abca8b5a928b552fa066
vthuong170599 <vuvanthuong170599@gmail.com>
2021-03-25 17:09:33 +0700

    add model and migration member role project task

---
+++ database/migrations/2021_03_25_091847_create_tasks_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject');
+            $table->tinyInteger('status');
+            $table->text('content');
+            $table->integer('priority');
+            $table->string('spent_time');
+            $table->string('estimated_time');
+            $table->dateTime('start_date');
+            $table->dateTime('due_date');
+            $table->bigInteger('member_id');
+            $table->bigInteger('id_project');
+            $table->text('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
