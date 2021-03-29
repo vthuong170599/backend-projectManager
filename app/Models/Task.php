37be15376a3ffa1c151ff4ee7b02a7607d7b7b2d
vthuong170599 <vuvanthuong170599@gmail.com>
2021-03-29 17:35:19 +0700

    api task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 class Task extends Model
 {
     use HasFactory;
     protected $fillable = ['subject', 'status', 'content', 'priority', 'spent_time', 'estimated_time', 'start_date','due_date', 'member_id', 'id_project', 'note'];
+
+    public function Member(){
+        return $this->belongsTo(User::class,'member_id');
+    }
+
+    public function Project(){
+        return $this->belongsTo(Project::class,'id_project');
+    }
 }
