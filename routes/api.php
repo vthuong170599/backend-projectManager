53e5755e9374f96b567ee64e4ed66b952c1640a5
khangkhoai <khangnguyen2311998@gmail.com>
2021-03-30 14:26:47 +0700

    add user - khang

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoleController;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 
@@ -23,34 +23,6 @@ use App\Http\Controllers\TaskController;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::group([
-    'prefix' => 'auth'
-], function () {
-    Route::post('login', 'AuthController@login');
-    Route::post('signup', 'AuthController@signup');
-
-    Route::group([
-      'middleware' => 'auth:api'
-    ], function() {
-        Route::get('logout', 'AuthController@logout');
-        Route::get('user', 'AuthController@user');
-    });
-});
-Route::group([
-    'prefix' => 'auth'
-], function () {
-    Route::post('login', [AuthController::class,'login']);
-    Route::post('signup', [AuthController::class,'signup']);
-
-    Route::group([
-      'middleware' => 'auth:api'
-    ], function() {
-        Route::get('logout', [UserController::class,'logout']);
-        Route::get('user', [UserController::class,'user']);
-    });
-});
-Route::resource('roles',RoleController::class);
-
 
 Route::get('projects',[ProjectController::class,'index']);
 Route::get('projects/{id}', [ProjectController::class,'show']);
@@ -59,6 +31,7 @@ Route::put('projects/{id}', [ProjectController::class,'update']);
 Route::delete('projects/{id}', [ProjectController::class,'delete']);
 Route::get('project', [ProjectController::class,'search']);
 Route::get('all-user', [UserController::class,'getAllUser']);
+
 
 Route::prefix('task')->group(function () {
     Route::get('','TaskController@index');
@@ -74,6 +47,8 @@ Route::prefix('roles')->group(function () {
     Route::get('/{id}','RoleController@show');
     Route::put('/{id}','RoleController@update');
     Route::delete('/{id}','RoleController@destroy');
-
-   
+});
+Route::prefix('user')->group(function () {
+    Route::post('','UserController@store');
+    
 });
